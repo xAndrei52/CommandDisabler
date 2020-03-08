7505d40fbbdf6a699438f721264d29b3628fe067
Molly <57105640+Bumbumkill@users.noreply.github.com>
2020-03-08 22:14:17 +0700

    Create Main.php

---
+++ src/Bumbumkill/CommandDisabler/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bumbumkill\CommandDisabler;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+
+public $cmd
+public function onEnable(){
+    @mkdir($this->getDataFolder());
+		$this->saveResource("commands.yml");
+		$data = new Config($this->getDataFolder()."commands.yml", Config::YAML);
+		$this->cmd = $data->get("cmd");
+		
+	}
+public function disableCommand(){
+		$cmp = $this->getServer()->getCommandMap();
+		foreach($this->cmd as $cmdlist){
+			$result = $map->getCommand($cmdname);
+
+				$cmp->unregister($result);
+
+		}
+		return true;
+	}
+}
